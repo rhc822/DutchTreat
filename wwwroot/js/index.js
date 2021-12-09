355e26ab9f1d648089b649d35cd2d931a16a68a5
Ryan Cunningham <ryan.cunningham@acadiahealthcare.com>
2021-12-09 16:10:29 -0600

    continuing Module 4

---
+++ wwwroot/js/index.js
@@ -0,0 +1,18 @@
+﻿var x = 0;
+var s = "";
+
+console.log("Hi");
+
+var theForm = document.getElementById("theForm");
+theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+    console.log("Buying Item");
+});
+
+var productInfo = document.getElementsByClassName("product-props");
+var li = document.getElementsByTagName("li");
+var listItems = li.item(3).children;
+console.log(listItems);
+//var listItems = console.log(typeof productInfo.item)
