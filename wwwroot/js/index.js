c53df22c68826c7eb2d2054cfac6c23fc3a713e3
Ryan Cunningham <ryan.cunningham@acadiahealthcare.com>
2021-12-10 08:58:40 -0600

    continuing to work on Module 4

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -3,16 +3,45 @@ var s = "";
 
 console.log("Hi");
 
+// Vanilla JS
+/*
 var theForm = document.getElementById("theForm");
 theForm.hidden = true;
+*/
 
+// jQuery
+
+var theForm = $("#theForm");
+theForm.hide();
+
+
+// Vanilla JS
+/*
 var button = document.getElementById("buyButton");
 button.addEventListener("click", function () {
     console.log("Buying Item");
 });
+*/
 
-var productInfo = document.getElementsByClassName("product-props");
-var li = document.getElementsByTagName("li");
-var listItems = li.item(3).children;
-console.log(listItems);
-//var listItems = console.log(typeof productInfo.item)
+// jQuery
+var button = $("buyButton");
+button.on("click", function () {
+    console.log("Buying Item");
+});
+
+// Vanilla JS
+/*
+var productInfo = document.getElementsByClassName(".product-props");
+var listItems = productInfo.item[0].children;
+*/
+
+//jQuery
+
+/*
+  jQuery enables full CSS selector syntax in a query(e.g. 'li' in product info below), instead of looping through the object
+*/
+var productInfo = $(".product-props li");
+productInfo.on("click", function () {
+    console.log("You clicked on " + $(this).text());
+});
+
