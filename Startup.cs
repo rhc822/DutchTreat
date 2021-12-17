7314aa16cf3e81222db4fbe7c413cbf1c715a1f0
Ryan Cunningham <ryan.cunningham@acadiahealthcare.com>
2021-12-17 08:53:15 -0600

    Continuing module 5...

--- Startup.cs
+++ Startup.cs
@@ -21,8 +21,9 @@ namespace DutchTreat
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles();
             app.UseStaticFiles();
+
+            app.UseRouting();
         }
     }
 }
