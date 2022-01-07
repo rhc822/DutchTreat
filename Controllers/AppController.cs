a12f0698ec1b19d4c5b7ca817c500316c0750288
Ryan Cunningham <ryan.cunningham@acadiahealthcare.com>
2022-01-07 09:20:38 -0600

    Continuing Module 5: starting on Model Binding section

--- Controllers/AppController.cs
+++ Controllers/AppController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -40,7 +41,7 @@ namespace DutchTreat.Controllers
         }
 
         [HttpPost("contact")]
-        public IActionResult Contact(object model)
+        public IActionResult Contact(ContactViewModel model)
         {
             return View();
         }
