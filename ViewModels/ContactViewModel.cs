d22a339b67e51b505a04b348ecfa570b458b5909
Ryan Cunningham <ryan.cunningham@acadiahealthcare.com>
2022-01-20 09:07:16 -0600

    Continuing Module 5: added client-side field validation and removed comments

--- ViewModels/ContactViewModel.cs
+++ ViewModels/ContactViewModel.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace DutchTreat.ViewModels
 {
+    /*
+     * HELP: Client-side validation doesn't work for Name field, MinLength ("Required" DOES work for both)
+     */
+
     public class ContactViewModel
     {
         [Required]
@@ -17,7 +21,7 @@ namespace DutchTreat.ViewModels
         [Required]
         public string Subject { get; set; }
         [Required]
-        [MaxLength(50)]
+        [MaxLength(50, ErrorMessage ="Message too long")]
         public string Message { get; set; }
     }
 }
