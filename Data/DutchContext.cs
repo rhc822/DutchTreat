7d2031c838b7b58272aa09653b80319f3060ce07
Ryan Cunningham <ryan.cunningham@acadiahealthcare.com>
2022-03-07 08:36:00 -0600

    Aligned DutchContext.cs to Shawn's file in module 7 (though it changed without explanation). Also, in DutchTreat.csproj enabled the whole module 7 course files to be viewed (and thus compared), minus the SLN and CSPROJ files

--- Data/DutchContext.cs
+++ Data/DutchContext.cs
@@ -10,20 +10,37 @@ namespace DutchTreat.Data
 {
     public class DutchContext : DbContext
     {
-        private readonly IConfiguration _config;
+        //private readonly IConfiguration _config;
 
-        public DutchContext(IConfiguration config)
+        //public DutchContext(IConfiguration config)
+        //{
+        //    _config = config;
+        //}
+
+        public DutchContext(DbContextOptions<DutchContext> options): base(options)
         {
-            _config = config;
+
         }
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    base.OnConfiguring(optionsBuilder);
 
-            optionsBuilder.UseSqlServer(_config["ConnectionStrings:DutchContextDb"]);
+        //    optionsBuilder.UseSqlServer(_config["ConnectionStrings:DutchContextDb"]);
+        //}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Order>()
+                .HasData(new Order()
+                {
+                    Id = 1,
+                    OrderDate = DateTime.UtcNow,
+                    OrderNumber = "12345"
+                });
         }
     }
 }
